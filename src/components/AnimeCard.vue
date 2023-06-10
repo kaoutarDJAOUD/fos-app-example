edbf80909bed3d755eb9020a15f694e40a238c38
KOUDJIL Khayri Anis <koudjilkhayrianis@gmail.com>
2023-06-10 01:49:33 +0100

    add style

--- src/components/AnimeCard.vue
+++ src/components/AnimeCard.vue
@@ -1,22 +1,20 @@
 <template>
-  <b-card
-    :title="anime.title"
-    :img-src="anime.imgUrl"
-    :img-alt="`${anime.title}-Image`"
-    img-top
-    style="max-width: 20rem"
-    class="mb-2"
-  >
+  <b-card :title="title" :img-src="anime.imgUrl" :img-alt="`${anime.title}-Image`" img-top style="max-width: 20rem"
+    class="mb-2 text-center ">
     <b-card-text> Number of episode : {{ anime.episodes }} </b-card-text>
     <b-card-text> Status : {{ anime.status }} </b-card-text>
 
-    <b-button href="#" variant="outline-primary">Edit</b-button>
+    <b-button class="btn btn-outline-primary m-2" @click="navigateToEdit" variant="outline-primary">Edit</b-button>
   </b-card>
 </template>
 
 <script>
 export default {
   props: {
+    index: {
+      type: Number,
+      default: -1
+    },
     anime: {
       type: Object,
       default: function () {
@@ -29,8 +27,22 @@ export default {
       },
     },
   },
+
+  methods: {
+    navigateToEdit() {
+      const data = {
+        index: this.index,
+        anime: this.anime
+      };
+
+      this.$router.push({
+        name: 'Edit Anime',
+        query: data
+      });
+    }
+
+  },
 };
 </script>
 
-<style>
-</style>
+<style></style>
--- src/components/AnimeForm.vue
+++ src/components/AnimeForm.vue
@@ -2,49 +2,25 @@
   <div>
     <b-form @submit="onSubmit" @reset="onReset" v-if="show">
       <b-form-group id="input-group-1" label="Title:" label-for="input-1">
-        <b-form-input
-          id="input-1"
-          v-model="form.title"
-          type="text"
-          placeholder="Enter the title"
-          required
-        ></b-form-input>
+        <b-form-input id="input-1" v-model="form.title" type="text" placeholder="Enter the title" required></b-form-input>
       </b-form-group>
 
-      <b-form-group
-        id="input-group-2"
-        label="Number of episodes:"
-        label-for="input-2"
-      >
-        <b-form-input
-          id="input-2"
-          v-model="form.episodes"
-          placeholder="Enter number of episodes"
-          type="number"
-          min="0"
-        ></b-form-input>
+      <b-form-group id="input-group-2" label="Number of episodes:" label-for="input-2">
+        <b-form-input id="input-2" v-model="form.episodes" placeholder="Enter number of episodes" type="number"
+          min="0"></b-form-input>
       </b-form-group>
 
       <b-form-group id="input-group-3" label="Status:" label-for="input-3">
-        <b-form-select
-          id="input-3"
-          v-model="form.status"
-          :options="statusOptions"
-          required
-        ></b-form-select>
+        <b-form-select id="input-3" v-model="form.status" :options="statusOptions" required></b-form-select>
       </b-form-group>
 
       <b-form-group id="input-group-4" label="Image url:" label-for="input-4">
-        <b-form-input
-          id="input-4"
-          v-model="form.imgUrl"
-          placeholder="Enter an url for the cover image"
-          type="url"
-        ></b-form-input>
+        <b-form-input id="input-4" v-model="form.imgUrl" placeholder="Enter an url for the cover image"
+          type="url"></b-form-input>
       </b-form-group>
+      <b-button class="mr-2 mt-1" type="submit" variant="primary">Submit</b-button>
+      <b-button class="ml-2 mt-1" type="reset" variant="danger">Reset</b-button>
 
-      <b-button type="submit" variant="primary">Submit</b-button>
-      <b-button type="reset" variant="danger">Reset</b-button>
     </b-form>
     <b-card class="mt-3" header="Form Data Result">
       <pre class="m-0">{{ form }}</pre>
@@ -93,5 +69,4 @@ export default {
 };
 </script>
 
-<style>
-</style>
+<style></style>
